932fc254c7c5a18be78032ea3f9f663d7c0bf8d4
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-05-07 07:40:49 +0800

    first time start react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var style = {
+    color: "red",
+    backgroundColor: "yellow",
+    padding: "px",
+    borderRadius: "5px",
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>I am react Person</h2>
+        <Person name="Munna" job="Football" ticket="456"></Person>
+        <Person name="Arham" job="Dorshok" ticket="987"></Person>
+        <Person name="Masud" job="Refarey" ticket="876"></Person>
+        <Person name="Rana"  job="Bite" ticket="543"></Person>
       </header>
+    </div>
+  );
+}
+
+function Person(props) {
+  return (
+    <div style={{ border: "1px solid yellow", width: "400px", margin: "10px" }}>
+      <h3>My name: {props.name}</h3>
+      <p>My profession: {props.job}</p>
+      <p><em>Ticket: {props.ticket}</em></p>
     </div>
   );
 }
